d25a6d5b7cbd924c18c38adf56aff12412272e8a
Simon Rogers <simonrogers@gmlconsulting.co.uk>
2017-04-04 11:41:59 +0100

    Check if functions exist

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -2,40 +2,44 @@
 
 namespace Midnite81\Helpers;
 
-/**
- * Concatenate strings together
- *
- * @return string
- */
-function concatenate()
-{
-    $args = func_get_args();
-    if (! empty($args)) {
-        foreach($args as $key=>$arg) {
-            if (is_object($args) || is_array($arg)) {
-                unset($args[$key]);
+if (!function_exists('concatenate')) {
+    /**
+     * Concatenate strings together
+     *
+     * @return string
+     */
+    function concatenate()
+    {
+        $args = func_get_args();
+        if (!empty($args)) {
+            foreach ($args as $key => $arg) {
+                if (is_object($args) || is_array($arg)) {
+                    unset($args[$key]);
+                }
             }
         }
+        return implode('', $args);
     }
-    return implode('', $args);
 }
 
-/**
- * Concatenate strings with specified separator as first argument
- *
- * @return string
- */
-function concatenate_with_separator()
-{
+if (! function_exists('concatenate_with_separator')) {
+    /**
+     * Concatenate strings with specified separator as first argument
+     *
+     * @return string
+     */
+    function concatenate_with_separator()
+    {
         $args = func_get_args();
         $separator = (isset($args[0])) ? $args[0] : ' ';
         unset($args[0]);
-        if (! empty($args)) {
-            foreach($args as $key=>$arg) {
+        if (!empty($args)) {
+            foreach ($args as $key => $arg) {
                 if (is_object($args) || is_array($arg)) {
                     unset($args[$key]);
                 }
             }
         }
         return implode($separator, $args);
+    }
 }
