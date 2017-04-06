1e4e3cdafef1f9fea1b052946fdc14219d4aebe0
Simon Rogers <simonrogers@gmlconsulting.co.uk>
2017-04-06 14:14:08 +0100

    Add get env variable command

---
+++ src/Commands/GetEnvVariable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Midnite81\LaravelBase\Commands;
+
+use Illuminate\Console\Command;
+
+class GetEnvVariable extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'env:get 
+                            {key}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Gets a value in the .env';
+
+    /**
+     * Create a new command instance.
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        return env($this->argument('key'));
+    }
+}
