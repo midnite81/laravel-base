fe5bf89daad4b33c6cb02c486671496fd3e89d49
Simon Rogers <simonrogers@gmlconsulting.co.uk>
2018-02-22 09:32:05 +0000

    Make Date Class

---
+++ src/Services/Date.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Midnite81\LaravelBase\Services;
+
+class Date
+{
+    public $dmy = "d/m/Y";
+    public $ymd = "Y/m/d";
+    public $mdy = "m/d/Y";
+    public $db = "Y-m-d";
+}
