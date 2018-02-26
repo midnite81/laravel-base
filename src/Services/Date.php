7aec5eedd06c2acfb248de48a8ad53097fb855f6
Simon Rogers <simonrogers@gmlconsulting.co.uk>
2018-02-26 10:10:34 +0000

    Update dates

--- src/Services/Date.php
+++ src/Services/Date.php
@@ -2,10 +2,15 @@
 
 namespace Midnite81\LaravelBase\Services;
 
+use DateTime;
+
 class Date
 {
     const DMY = "d/m/Y";
     const YMD = "Y/m/d";
     const MDY = "m/d/Y";
     const DB = "Y-m-d";
+    const FULLTZ = "Y-m-d\TH:i:s:v\Z";
+    const FULLTZ_DOT= "Y-m-d\TH:i:s.u\Z";
+    const ATOM = 'Y-m-d\TH:i:sP';
 }
